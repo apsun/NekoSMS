7c51f6414c20b817bdd34b95009448c20eab57be
Andrew Sun <youlosethegame@live.com>
2015-03-26 21:35:34 +0800

    Starting to add filter editing (basic functionality working)

--- src/com/oxycode/nekosms/app/FilterEditorActivity.java
+++ src/com/oxycode/nekosms/app/FilterEditorActivity.java
@@ -1,44 +1,122 @@
 package com.oxycode.nekosms.app;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 import android.widget.Toolbar;
 import com.oxycode.nekosms.R;
+import com.oxycode.nekosms.data.SmsFilterField;
+import com.oxycode.nekosms.data.SmsFilterMode;
+import com.oxycode.nekosms.provider.NekoSmsContract;
 
 public class FilterEditorActivity extends Activity {
+    public static final String EXTRA_FILTER_ID = "filterId";
+
+    private EditText mPatternEditText;
+    private Spinner mFieldSpinner;
+    private Spinner mModeSpinner;
+    private long mFilterId;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter_editor);
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_done_white_24dp);
-        toolbar.setTitle("Add new filter");
+
+        Intent intent = getIntent();
+        long filterId = intent.getLongExtra(EXTRA_FILTER_ID, -1);
+
+        mFilterId = filterId;
+        mPatternEditText = (EditText)findViewById(R.id.activity_filter_editor_pattern_edittext);
+        mFieldSpinner = (Spinner)findViewById(R.id.activity_filter_editor_field_spinner);
+        mModeSpinner = (Spinner)findViewById(R.id.activity_filter_editor_mode_spinner);
+
+        if (filterId >= 0) {
+            Cursor cursor = getFilterData();
+            cursor.moveToFirst();
+            SmsFilterField field = SmsFilterField.valueOf(cursor.getString(0));
+            SmsFilterMode mode = SmsFilterMode.valueOf(cursor.getString(1));
+            String pattern = cursor.getString(2);
+            cursor.close();
+            // TODO: Set field & mode
+            mPatternEditText.setText(pattern);
+            toolbar.setTitle("Save filter");
+        } else {
+            toolbar.setTitle("Add new filter");
+        }
+
         setActionBar(toolbar);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_filter_editor, menu);
+        inflater.inflate(R.menu.options_filter_editor, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case android.R.id.home:
-                Toast.makeText(this, "Filter saved", Toast.LENGTH_SHORT).show();
-                finish();
-                return true;
-            case R.id.menu_item_discard_changes:
-                finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        case android.R.id.home:
+            writeFilterData();
+            Toast.makeText(this, "Filter saved", Toast.LENGTH_SHORT).show();
+            finish();
+            return true;
+        case R.id.menu_item_discard_changes:
+            finish();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private Cursor getFilterData() {
+        ContentResolver contentResolver = getContentResolver();
+        Uri filtersUri = NekoSmsContract.Filters.CONTENT_URI;
+        Uri filterUri = ContentUris.withAppendedId(filtersUri, mFilterId);
+        String[] projection = {
+            NekoSmsContract.Filters.FIELD,
+            NekoSmsContract.Filters.MODE,
+            NekoSmsContract.Filters.PATTERN
+        };
+        return contentResolver.query(filterUri, projection, null, null, null);
+    }
+
+    private ContentValues createFilterData() {
+        ContentValues values = new ContentValues();
+        SmsFilterField field = SmsFilterField.valueOf((String)mFieldSpinner.getSelectedItem());
+        SmsFilterMode mode = SmsFilterMode.valueOf((String)mModeSpinner.getSelectedItem());
+        String pattern = mPatternEditText.getText().toString();
+        values.put(NekoSmsContract.Filters.MODE, mode.name());
+        values.put(NekoSmsContract.Filters.FIELD, field.name());
+        values.put(NekoSmsContract.Filters.PATTERN, pattern);
+        return values;
+    }
+
+    private void writeFilterData() {
+        ContentResolver contentResolver = getContentResolver();
+        ContentValues values = createFilterData();
+        Uri filtersUri = NekoSmsContract.Filters.CONTENT_URI;
+        if (mFilterId >= 0) {
+            Uri filterUri = ContentUris.withAppendedId(filtersUri, mFilterId);
+            int updatedRows = contentResolver.update(filterUri, values, null, null);
+            // TODO: Check return value
+        } else {
+            Uri filterUri = contentResolver.insert(filtersUri, values);
+            // TODO: Check return value
         }
     }
 }
--- src/com/oxycode/nekosms/app/FilterListActivity.java
+++ src/com/oxycode/nekosms/app/FilterListActivity.java
@@ -1,49 +1,118 @@
 package com.oxycode.nekosms.app;
 
-import android.app.Activity;
-import android.content.ContentValues;
+import android.app.ListActivity;
+import android.app.LoaderManager;
+import android.content.*;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.view.*;
+import android.widget.AdapterView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Toolbar;
 import com.oxycode.nekosms.R;
 import com.oxycode.nekosms.provider.NekoSmsContract;
 
-public class FilterListActivity extends Activity {
+public class FilterListActivity extends ListActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+    private SimpleCursorAdapter mAdapter;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter_list);
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setActionBar(toolbar);
+
+        LoaderManager loaderManager = getLoaderManager();
+        loaderManager.initLoader(0, null, this);
+
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+            this, R.layout.listitem_filter_list, null,
+            new String[] { NekoSmsContract.Filters.PATTERN, NekoSmsContract.Filters.FIELD },
+            new int[] { R.id.listitem_filter_list_pattern_textview, R.id.listitem_filter_list_info_textview }, 0);
+        setListAdapter(adapter);
+
+
+        registerForContextMenu(getListView());
+
+        mAdapter = adapter;
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_filter_list, menu);
+        inflater.inflate(R.menu.options_filter_list, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.menu_item_create_filter:
-                // TODO: Testing code, remove when done
-                ContentValues v = new ContentValues();
-                v.put(NekoSmsContract.Filters.FIELD, "BODY");
-                v.put(NekoSmsContract.Filters.MODE, "REGEX");
-                v.put(NekoSmsContract.Filters.PATTERN, "^123");
-                getContentResolver().insert(NekoSmsContract.Filters.CONTENT_URI, v);
-
-                // Intent filterEditorIntent = new Intent(this, FilterEditorActivity.class);
-                // startActivity(filterEditorIntent);
-                return true;
-            case R.id.menu_item_import_from_sms:
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        case R.id.menu_item_create_filter:
+            startFilterEditorActivity(-1);
+            return true;
+        case R.id.menu_item_import_from_sms:
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.context_filter_list, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+        long rowId = info.id;
+        switch (item.getItemId()) {
+        case R.id.contextmenu_filter_list_edit:
+            startFilterEditorActivity(rowId);
+            return true;
+        case R.id.contextmenu_filter_list_delete:
+            deleteFilter(rowId);
+            return true;
+        default:
+            return super.onContextItemSelected(item);
+        }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String[] from = {
+            NekoSmsContract.Filters._ID,
+            NekoSmsContract.Filters.PATTERN,
+            NekoSmsContract.Filters.MODE,
+            NekoSmsContract.Filters.FIELD
+        };
+        Uri uri = NekoSmsContract.Filters.CONTENT_URI;
+        return new CursorLoader(this, uri, from, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mAdapter.changeCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mAdapter.changeCursor(null);
+    }
+
+    private void startFilterEditorActivity(long filterId) {
+        Intent filterEditorIntent = new Intent(this, FilterEditorActivity.class);
+        filterEditorIntent.putExtra(FilterEditorActivity.EXTRA_FILTER_ID, filterId);
+        startActivity(filterEditorIntent);
+    }
+
+    private void deleteFilter(long filterId) {
+        ContentResolver contentResolver = getContentResolver();
+        Uri filtersUri = NekoSmsContract.Filters.CONTENT_URI;
+        Uri filterUri = ContentUris.withAppendedId(filtersUri, filterId);
+        int deletedRows = contentResolver.delete(filterUri, null, null);
+        // TODO: Check return value
     }
 }
