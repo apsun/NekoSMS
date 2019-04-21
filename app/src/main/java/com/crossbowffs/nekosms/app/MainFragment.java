fba87898557eb4b70fc9e46a28613d54760558a3
Andrew Sun <0xandrewsun@gmail.com>
2019-04-20 20:20:58 -0700

    Delete unused permission request code

--- app/src/main/java/com/crossbowffs/nekosms/app/MainFragment.java
+++ app/src/main/java/com/crossbowffs/nekosms/app/MainFragment.java
@@ -2,17 +2,11 @@ package com.crossbowffs.nekosms.app;
 
 import android.app.Fragment;
 import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
-import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.widget.Toast;
-import com.crossbowffs.nekosms.utils.PermissionUtils;
 
 public class MainFragment extends Fragment {
     public MainActivity getMainActivity() {
@@ -62,63 +56,6 @@ public class MainFragment extends Fragment {
 
     public void disableFab() {
         getMainActivity().disableFab();
-    }
-
-    public void requestPermissionsCompat(String permission, int requestCode, boolean openSettings) {
-        requestPermissionsCompat(new String[] {permission}, requestCode, openSettings);
-    }
-
-    private void openPermissionSettings(int requestCode) {
-        Intent intent = new Intent();
-        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-        Uri uri = Uri.fromParts("package", getContext().getPackageName(), null);
-        intent.setData(uri);
-        startActivityForResult(intent, requestCode);
-    }
-
-    public void requestPermissionsCompat(String[] permissions, int requestCode, boolean openSettings) {
-        // Unlike requestPermissions(), this will not display the request dialog
-        // on Android 23+ if the permissions have already been granted.
-        int[] grantResults = new int[permissions.length];
-        boolean hasPermissions = PermissionUtils.checkPermissions(getContext(), permissions, grantResults);
-        if (!hasPermissions && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            // First check if user has ticked the "never ask again" option.
-            // If so (and openSettings is true), open the settings activity instead.
-            // Note that this will NOT call onRequestPermissionsResult.
-            if (openSettings) {
-                for (String permission : permissions) {
-                    if (!shouldShowRequestPermissionRationale(permission)) {
-                        openPermissionSettings(requestCode);
-                        return;
-                    }
-                }
-            }
-
-            requestPermissions(permissions, requestCode);
-        } else {
-            // If we already have the permissions (or we're on pre-M),
-            // just directly call the result handler
-            onRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        for (int permissionStatus : grantResults) {
-            if (permissionStatus != PackageManager.PERMISSION_GRANTED) {
-                onRequestPermissionsResult(requestCode, false);
-                return;
-            }
-        }
-
-        // Empty permissions means that the request was cancelled
-        // by the user, so the permissions were not granted
-        boolean requestCancelled = (grantResults.length == 0);
-        onRequestPermissionsResult(requestCode, !requestCancelled);
-    }
-
-    protected void onRequestPermissionsResult(int requestCode, boolean granted) {
-
     }
 
     public void onNewArguments(Bundle args) {
