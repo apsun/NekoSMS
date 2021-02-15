82205ebed9cd57fd59a59457d2a65b10a5e17751
Andrew Sun <andrew@crossbowffs.com>
2021-02-14 16:13:30 -0800

    build.gradle -> Kotlin

---
+++ app/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    id("com.android.application")
+}
+
+dependencies {
+    implementation("androidx.core:core:1.3.2")
+    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.recyclerview:recyclerview:1.1.0")
+    implementation("androidx.annotation:annotation:1.1.0")
+
+    // Material design library is a complete PoS that has reliably fixed and introduced a
+    // visible bug in every version, so lock ourselves to this alpha version that is new
+    // enough to have the disappearing icon bug fixed but old enough to not have the
+    // broken snackbar/FAB animation that was added in 1.1.0-alpha10.
+    // See: https://github.com/material-components/material-components-android/issues/1036
+    implementation("com.google.android.material:material:1.1.0-alpha09")
+
+    implementation("com.crossbowffs.remotepreferences:remotepreferences:0.8")
+    compileOnly("de.robv.android.xposed:api:53")
+}
+
+android {
+    compileSdkVersion(30)
+
+    defaultConfig {
+        versionCode(36)
+        versionName("0.18.1-dev")
+        minSdkVersion(19)
+        targetSdkVersion(30)
+        resConfigs("en", "zh-rCN")
+        buildConfigField("int", "MODULE_VERSION", "17")
+        buildConfigField("int", "DATABASE_VERSION", "11")
+        buildConfigField("int", "BACKUP_VERSION", "3")
+        buildConfigField("String", "LOG_TAG", "\"NekoSMS\"")
+    }
+
+    buildTypes {
+        getByName("debug") {
+            buildConfigField("int", "LOG_LEVEL", "2")
+            buildConfigField("boolean", "LOG_TO_XPOSED", "false")
+        }
+
+        getByName("release") {
+            postprocessing {
+                isRemoveUnusedCode = true
+                isRemoveUnusedResources = true
+                isObfuscate = false
+                isOptimizeCode = true
+                proguardFile("proguard-rules.pro")
+            }
+            buildConfigField("int", "LOG_LEVEL", "4")
+            buildConfigField("boolean", "LOG_TO_XPOSED", "true")
+        }
+    }
+
+    lintOptions {
+        isAbortOnError = false
+    }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,19 @@
 buildscript {
     repositories {
         google()
+        mavenCentral()
         jcenter()
     }
 
     dependencies {
-        classpath 'com.android.tools.build:gradle:4.0.0'
+        classpath("com.android.tools.build:gradle:4.1.2")
     }
 }
 
 allprojects {
     repositories {
         google()
+        mavenCentral()
         jcenter()
     }
 }
