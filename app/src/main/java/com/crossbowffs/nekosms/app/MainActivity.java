97223b1cc0bcb8be9df472b66c0cbfbbc423c7d3
Andrew Sun <andrew@crossbowffs.com>
2022-12-10 19:00:07 -0800

    Remove unused import

--- app/src/main/java/com/crossbowffs/nekosms/app/MainActivity.java
+++ app/src/main/java/com/crossbowffs/nekosms/app/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
-import android.preference.RingtonePreference;
 import android.view.View;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
