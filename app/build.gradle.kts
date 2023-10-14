05d5bafb9dca55efcb6be90d0ca443c49883fca1
Andrew Sun <andrew@crossbowffs.com>
2023-10-13 20:57:51 -0700

    Update deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,28 +3,29 @@ plugins {
 }
 
 dependencies {
-    implementation("androidx.core:core:1.9.0")
+    implementation("androidx.core:core:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.annotation:annotation:1.5.0")
+    implementation("androidx.annotation:annotation:1.7.0")
     implementation("androidx.loader:loader:1.1.0")
-    implementation("androidx.fragment:fragment:1.5.7")
-    implementation("androidx.preference:preference:1.2.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.0")
+    implementation("androidx.fragment:fragment:1.6.1")
+    implementation("androidx.preference:preference:1.2.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.1")
     implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("com.crossbowffs.remotepreferences:remotepreferences:0.8")
     compileOnly("de.robv.android.xposed:api:53")
 }
 
 android {
-    compileSdk = 33
+    namespace = "com.crossbowffs.nekosms"
+    compileSdk = 34
 
     defaultConfig {
         versionCode = 41
         versionName = "0.22.0-dev"
         minSdk = 19
-        targetSdk = 33
+        targetSdk = 34
         resourceConfigurations.addAll(listOf("en", "zh-rCN", "ru"))
         buildConfigField("int", "MODULE_VERSION", "18")
         buildConfigField("int", "DATABASE_VERSION", "11")
